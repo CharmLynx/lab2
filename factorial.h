babac25d2a80ce84ff6902824ba2f53e244071b8
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-05-08 12:44:50 +0300

    head file

---
+++ factorial.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include <stdexcept>
+
+int factorial(int n) {
+    if (n < 0)
+        throw invalid_argument("not defined for negative numbers.");
+    if (floor(n)==n)
+        throw invalid_argument("defined for integers only");
+    if (n == 0)
+        return 1;
+    int result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
